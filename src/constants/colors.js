6e709a19f03ed0ee3d3ca66d9a6dd632efc9b2fb
lalalappochka <sanyalappo16@gmail.com>
2023-03-02 15:49:48 +0300

    initial commit

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    white: '#FFFFFF',
+    blue: '#2877ED',
+    orange: '#FF4C11',
+    green: '#00C4A2',
+}
+
+export default colors;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import colors from '../constants/colors'
+
+const Login = (props) =>{
+return (
+    <Container>
+        <h1>Page my page</h1>
+    </Container>
+)
+}
+
+const Container = styled.div`
+display: flex;
+background-color: ${colors.blue};
+`
+
+export default Login
+ 
